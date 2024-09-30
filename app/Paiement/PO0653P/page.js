1d722c24636035a35d65fcb361eaed6f76f1f723
yassinezadod <yassine.zadod@e-polytechnique.ma>
2024-09-30 01:14:04 +0100

    last commit deploy42AF

--- app/Paiement/PO0653P/page.js
+++ app/Paiement/PO0653P/page.js
@@ -5,7 +5,7 @@ import axios from 'axios'; // Ajout d'importation d'axios
 import {FaTrash, FaEdit } from 'react-icons/fa';
 import { Layout } from "antd";
 import Sidebar from '../../components/Sidebar';
-import NavBar from "../components/NavBar";
+import NavBar from "./Navbar";
 import { FaTimes } from 'react-icons/fa';
 import clsx from 'clsx'; // Ajout d'importation de clsx
 
